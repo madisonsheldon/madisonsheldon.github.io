d5599f5a2dddc3667d59126210ca37e97f27a616
madisonsheldon <71400841+madisonsheldon@users.noreply.github.com>
2020-12-06 17:53:54 -0500

    Lavender Project Updates
    Fixes to image on hover

---
+++ projects/p2-hyperlinked-set/script.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+	$('.text').hover (function (){
+  	$('.img').toggleClass('show');
+	});
+});
+
+$(document).ready(function(){
+	$('.textTwo').hover (function (){
+  	$('.imgTwo').toggleClass('show');
+	});
+});
+
+$(document).ready(function(){
+	$('.textThree').hover (function (){
+  	$('.imgThree').toggleClass('show');
+	});
+});
+
+$(document).ready(function(){
+	$('.textFour').hover (function (){
+  	$('.imgFour').toggleClass('show');
+	});
+});
+
+$(document).ready(function(){
+	$('.textFive').hover (function (){
+  	$('.imgFive').toggleClass('show');
+	});
+});
